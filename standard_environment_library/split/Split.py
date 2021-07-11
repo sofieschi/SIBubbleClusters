da400a8761fc129a3d9f9b4f40b48c9b84b50ad6
sofie <sofie.schi@gmail.com>
2021-07-11 23:39:00 +0200

    Split Tool straight line

--- standard_environment_library/split/Split.py
+++ standard_environment_library/split/Split.py
@@ -12,6 +12,14 @@ class Split(Deletable, Movable, SIEffect):
     region_display_name = "Split"
 
     def __init__(self, shape=PySI.PointVector(), uuid="", kwargs={}):
-        super(Split, self).__init__(shape, uuid, E.id.split_texture, Split.regiontype, Split.regionname, kwargs)
+        super(Split, self).__init__(Split.prepare_shape(shape), uuid, E.id.split_texture, Split.regiontype, Split.regionname, kwargs)
         self.set_QML_path("Split.qml")
         self.color = E.id.split_color
+
+    # the curved shape will be changed to a straight line
+    @staticmethod 
+    def prepare_shape(points):
+        new_shape = PySI.PointVector()
+        new_shape.append(points[0])
+        new_shape.append(points[len(points)-1])
+        return new_shape
